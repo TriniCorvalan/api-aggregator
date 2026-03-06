d8e26876a93dd90ac6b5c22de313b5eeb111bb73
trinicorvalan <tcorvalan@cobranzaonline.com>
2026-03-06 14:36:31 -0300

    refactor(user_status): update validations and add test coverage for user status creation

--- test/models/user_status_test.rb
+++ test/models/user_status_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class UserStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create user status" do
+    user_status = UserStatus.create(full_name: "John Doe", experience: "Rookie", pending_tasks_count: 0, next_urgent_task: "")
+    assert user_status.valid?
+  end
+
+  test "should not create user status without full name" do
+    user_status = UserStatus.create(experience: "Rookie", pending_tasks_count: 0, next_urgent_task: "")
+    assert_not user_status.valid?
+  end
+
+  test "should not create user status without experience" do
+    user_status = UserStatus.create(full_name: "John Doe", pending_tasks_count: 0, next_urgent_task: "")
+    assert_not user_status.valid?
+  end
+
+  test "should create user status without pending tasks count 0 as default" do
+    user_status = UserStatus.create(full_name: "John Doe", experience: "Rookie", next_urgent_task: "")
+    assert user_status.valid?
+    assert_equal user_status.pending_tasks_count, 0
+  end
+
+  test "should create user status with blank next urgent task" do
+    user_status = UserStatus.create(full_name: "John Doe", experience: "Rookie", pending_tasks_count: 0, next_urgent_task: "")
+    assert user_status.valid?
+  end
 end
